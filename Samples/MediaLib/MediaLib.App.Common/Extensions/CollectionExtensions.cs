34a934025a01d4eabb8716caa61df2c5d783556d
Franziska <Franziska@NB-TT-118888>
2017-03-08 11:10:57 +0100

    added missing source files

---
+++ Samples/MediaLib/MediaLib.App.Common/Extensions/CollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MediaLib.App.Common.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static void RemoveWhere<T>(this ICollection<T> collection, Func<T, bool> predicate)
+        {
+            foreach (var item in collection.Where(predicate).ToList())
+            {
+                collection.Remove(item);
+            }
+        }
+
+        public static void RemoveSingle<T>(this ICollection<T> collection, Func<T, bool> predicate)
+        {
+            collection.Remove(collection.Single(predicate));
+        }
+    }
+}
---
+++ Samples/MediaLib/MediaLib.App.Common/Extensions/MediaExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MediaLib.App.Common.Model;
+
+namespace MediaLib.App.Common.Extensions
+{
+    public static class MediaExtensions
+    {
+        public static Media Clone(this Media original)
+        {
+            var clone = new Media
+            {
+                Id = original.Id,
+                Title = original.Title,
+            };
+            return clone;
+        }
+    }
+}
